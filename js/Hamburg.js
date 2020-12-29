227d26799ab561b28ff8e1d3e107386cc9fa14e7
He Songlin <063>
2020-12-29 16:01:37 +0800

    10

--- js/Hamburg.js
+++ js/Hamburg.js
@@ -9,14 +9,16 @@
 //     }
 // }
 
+// 漢堡按鈕
 var HamburgBtn = document.getElementById("HamburgBtn");
 var showMenu = document.getElementById("showMenu");
 HamburgBtn.onclick = function () {
-    if (showMenu.style.display == "none") {
-        showMenu.style.display = "block";
-    } else {
-        showMenu.style.display = "none";
-    }
+    showMenu.style.display = "block";
+}
+
+// 漢堡按鈕關閉
+showMenu.onclick = function () {
+    showMenu.style.display = "none";
 }
 var H_close = document.getElementById("H_close");
 H_close.onclick = function(){
