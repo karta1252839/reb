c6c632fc147986bca5732e85e894476ba74961bc
He Songlin <063>
2021-01-21 17:26:09 +0800

    2

---
+++ js/GameIntro.js
@@ -0,0 +1,55 @@
+// 
+$(".card_1").mousemove(
+    function(){
+        $(this).attr('src','../img/Featurespage/GameIntro/cards/card1.png');
+    }
+)
+$(".card_1").mouseout(
+    function(){
+        $(this).attr('src','../img/Featurespage/GameIntro/cards/card 1 - off.png');
+    }
+)
+// 
+$(".card_2").mousemove(
+    function(){
+        $(this).attr('src','../img/Featurespage/GameIntro/cards/card2.png');
+    }
+)
+$(".card_2").mouseout(
+    function(){
+        $(this).attr('src','../img/Featurespage/GameIntro/cards/card 2 - off.png');
+    }
+)
+// 
+$(".card_3").mousemove(
+    function(){
+        $(this).attr('src','../img/Featurespage/GameIntro/cards/card3.png');
+    }
+)
+$(".card_3").mouseout(
+    function(){
+        $(this).attr('src','../img/Featurespage/GameIntro/cards/card 3 - off.png');
+    }
+)
+// 
+$(".card_4").mousemove(
+    function(){
+        $(this).attr('src','../img/Featurespage/GameIntro/cards/card4.png');
+    }
+)
+$(".card_4").mouseout(
+    function(){
+        $(this).attr('src','../img/Featurespage/GameIntro/cards/card 4 - off.png');
+    }
+)
+// 
+$(".card_5").mousemove(
+    function(){
+        $(this).attr('src','../img/Featurespage/GameIntro/cards/card5.png');
+    }
+)
+$(".card_5").mouseout(
+    function(){
+        $(this).attr('src','../img/Featurespage/GameIntro/cards/card 5 - off.png');
+    }
+)
